c825df9f6b3145c5b869673e53ee6cdd2ba127a2
HadjerChikouche <chikouche.hadjer@gmail.com>
2016-05-22 19:20:31 +0200

    adding two square move at first rule

--- src/com/company/MoteurDeRegles.java
+++ src/com/company/MoteurDeRegles.java
@@ -13,7 +13,59 @@ public class MoteurDeRegles {
 // if a square in front of him is occupied ,it can't move
 //can't capture it nor move over it *//
     public boolean pawnRules(String from,String to,String piece){
-        if (piece.substring(1).equals("P")){
+        if (piece.substring(1).equals("P")) {
+            return backwardsRule(from, to, piece) && singleMoveRule(from, to, piece);
+        }
+        return true;
+    }
+
+    private boolean twoSquaresAtFirst(String from, String to, String piece) {
+
+        int tO = Integer.valueOf(to.substring(1));
+        int fRom = Integer.valueOf(from.substring(1));
+
+        if(piece.substring(0).equals("WP")){
+
+            if (from.substring(1).equals("2") && tO-fRom==2){
+                return true;
+            }
+        }
+
+        if(piece.substring(0).equals("BP")){
+            if(from.substring(1).equals("7") && fRom- tO == 2){
+                return true;
+            }
+        }
+
+
+
+        return false;
+    }
+
+    private boolean singleMoveRule(String from, String to, String piece) {
+        if (twoSquaresAtFirst(from,to,piece)==true){
+            twoSquaresAtFirst(from,to,piece);
+        }else {
+            int tO = Integer.valueOf(to.substring(1));
+            int fRom = Integer.valueOf(from.substring(1));
+            if (piece.substring(0).equals("WP")) {
+
+                if (tO - fRom != 1) {
+                    return false;
+                }
+            }
+
+            if (piece.substring(0).equals("BP")) {
+                if (fRom - tO != 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean backwardsRule(String from, String to, String piece) {
             int tO = Integer.valueOf(to.substring(1));
             int fRom = Integer.valueOf(from.substring(1));
 
@@ -29,7 +81,6 @@ public class MoteurDeRegles {
                     return false;
                 }
             }
-        }
         return true;
     }
 }
